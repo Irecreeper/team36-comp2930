8d53569a60f6f3ec683e7df922917e24d8621f56
MDandaya@gmail.com <MDandaya@gmail.com>
2019-05-07 11:46:33 -0700

    adding branch with browser router

---
+++ ReactApp/src/components/hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> HELLO </h2>
+      </div>
+    )
+  }
+}
+
+export default Main
