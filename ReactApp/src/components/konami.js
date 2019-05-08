9c422d1b4c090151253613df9332e09db9ece6c0
Paolo Del Rio <pdelrio@csdr.ca>
2019-05-08 11:32:34 -0700

    oops accidentally on master

--- ReactApp/src/components/konami.js
+++ ReactApp/src/components/konami.js
@@ -1,57 +1,57 @@
 var key_seq = 0;
 
-            document.onkeydown = checkKey;
+document.onkeydown = checkKey;
 
-            function checkKey(e) {
-                e = e || window.event;
-            
-                if (e.keyCode == '38') { //up
-                    if (key_seq == 0 || key_seq == 1) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '40') { //down
-                    if (key_seq == 2 || key_seq == 3) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '37') { //left
-                    if (key_seq == 4 || key_seq == 6) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '39') { //right
-                    if (key_seq == 5 || key_seq == 7) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '65') { //a
-                    if (key_seq == 9) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '66') { //b
-                    if (key_seq == 8) {
-                        key_seq++;
-                    } else {
-                        key_seq = 0;
-                    }
-                } else if (e.keyCode == '13') { //enter / start
-                    if (key_seq == 10) {
-                        var audio = new Audio('js/ven_ven_ven.mp3');
-                        audio.play();
-                        key_seq = 0;
-                        console.log("VEN");
-                    } 
-                    key_seq = 0;
-                } else { //other
-                    key_seq = 0;
-                }
+function checkKey(e) {
+    e = e || window.event;
 
-                console.log(key_seq);
-            }
+    if (e.keyCode === '38') { //up
+        if (key_seq === 0 || key_seq === 1) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '40') { //down
+        if (key_seq === 2 || key_seq === 3) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '37') { //left
+        if (key_seq === 4 || key_seq === 6) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '39') { //right
+        if (key_seq === 5 || key_seq === 7) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '65') { //a
+        if (key_seq === 9) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '66') { //b
+        if (key_seq === 8) {
+            key_seq++;
+        } else {
+            key_seq = 0;
+        }
+    } else if (e.keyCode === '13') { //enter / start
+        if (key_seq === 10) {
+            var audio = new Audio('js/ven_ven_ven.mp3');
+            audio.play();
+            key_seq = 0;
+            console.log("VEN");
+        } 
+        key_seq = 0;
+    } else { //other
+        key_seq = 0;
+    }
+    
+    console.log(key_seq);
+}
