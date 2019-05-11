2cbc3ef2a027ee85313103a49dc4869afb7f041b
MDandaya@gmail.com <MDandaya@gmail.com>
2019-05-10 18:55:55 -0700

    adding build file in client to test on host

---
+++ client/build/precache-manifest.463ee3c34a300f6db0a70836a461bc4d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "95780eaee1de8c22ffc15fb2ad049a04",
+    "url": "/static/media/alec_mug.95780eae.png"
+  },
+  {
+    "revision": "9f627c4029173290078a",
+    "url": "/static/css/main.5ff4ae79.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2db36bb15cb637d93e5b",
+    "url": "/static/js/2.018f9164.chunk.js"
+  },
+  {
+    "revision": "840e24bbcd3db518a0cb50ff8320c507",
+    "url": "/static/media/ven_logo.840e24bb.png"
+  },
+  {
+    "revision": "45cad6b7b3b48f3fae90982a30e069f7",
+    "url": "/static/media/zach_mug.45cad6b7.png"
+  },
+  {
+    "revision": "9f627c4029173290078a",
+    "url": "/static/js/main.223151ee.chunk.js"
+  },
+  {
+    "revision": "a70b9b43c559855c1e06a20b2676d339",
+    "url": "/static/media/joel_mug.a70b9b43.png"
+  },
+  {
+    "revision": "cd8d61ce623c1b5aadab7929283d0925",
+    "url": "/static/media/manuel_mug.cd8d61ce.png"
+  },
+  {
+    "revision": "ca5c2f5fcf7b24cae82832081a960638",
+    "url": "/static/media/paolo_mug.ca5c2f5f.png"
+  },
+  {
+    "revision": "04bcefe511af7d1f96a000419258ff7b",
+    "url": "/static/media/ecostandards.04bcefe5.jpg"
+  },
+  {
+    "revision": "f4f751a605e8a5ded7806f15a73da982",
+    "url": "/static/media/forestbackground.f4f751a6.jpg"
+  },
+  {
+    "revision": "c3b44f941f76f100be136d32604d8a09",
+    "url": "/index.html"
+  }
+];
