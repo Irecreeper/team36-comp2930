f8533cdc17fba457d754d8ae6b7c8c8fcf629503
Alec Martin <acmartin1@live.ca>
2019-05-14 15:00:21 -0700

    fixed landing

--- client/src/components/landingpage.js
+++ client/src/components/landingpage.js
@@ -28,7 +28,6 @@ class LandingPage extends React.Component {
       <div className="text-center text-white">
         <div class="container2">
           <div class="panel top">
-            <p>{this.state.apiResponse}</p>
             <main role="main" className="cover">
               <Image
                 src={Logo}
@@ -42,7 +41,10 @@ class LandingPage extends React.Component {
               <h2 class="text-on-background-lg font-el hide-sm">
                 Your local news aggregate for all things green
               </h2>
-              <div id="landing-navigate" className="container is-narrow hide-sm">
+              <div
+                id="landing-navigate"
+                className="container is-narrow hide-sm"
+              >
                 <div className="row border-between">
                   <div className="col-md-4">
                     <h3 className="font-ro">View Articles</h3>
@@ -61,9 +63,11 @@ class LandingPage extends React.Component {
                 </div>
               </div>
 
-              <div className="show-sm"> {/* Only appears at small sizes. */}
+              <div className="show-sm">
+                {' '}
+                {/* Only appears at small sizes. */}
                 <View_art_but />
-                <br/>
+                <br />
                 <About_us_but_lp />
               </div>
 
@@ -74,7 +78,6 @@ class LandingPage extends React.Component {
                 <p className="text-on-background-sm font-ro">
                   Powered by News API
                 </p>
-                
               </footer>
             </main>
           </div>
