6f2d03aef4394e461fdbfc4fb1007fa2315ada54
Joel Colussi <joelcolussibcit@gmail.com>
2019-05-21 14:38:38 -0700

    Infinite scroll properly implemented

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -26,13 +26,16 @@ const style = {
 class Main extends React.Component {
   state = {
     items: Array.from({ length: 2 }),
+    hasMore: true
   }
 
   componentDidMount() {
     this.getDbArticles().then(data => {
       console.log(data);
       this.setState({articles: data});
+      this.setState({articleCount: this.state.articles.length});
       console.log(this.state.articles);
+      console.log(this.state.articleCount);
     })
   }
 
@@ -41,6 +44,12 @@ class Main extends React.Component {
       this.setState({
         items: this.state.items.concat(Array.from({ length: 2 })),
       })
+      console.log(this.state.items.length*3);
+      console.log(this.state.articleCount - 6)
+      console.log(this.state.hasMore)
+      if (this.state.items.length*3 > this.state.articleCount - 6) {
+        this.setState({hasMore: false});
+      }
     }, 500)
   }
 
@@ -63,29 +72,49 @@ class Main extends React.Component {
 
   render() {
     let articles = this.state.articles;
+    var articleCount = this.state.articleCount;
     return (
       <div id="container">
         <NavBar />
         <InfiniteScroll
           dataLength={this.state.items.length}
           next={this.fetchMoreData}
-          hasMore={true}
+          hasMore={this.state.hasMore}
           loader={<h4>Loading...</h4>}
         >
           {this.state.items.map((i, index) => (
             <div key={index}>
               <div class="container">
                 <div id="news-card-area" class="row">
-                { this.state && this.state.articles && 
-                  articles.slice(0).reverse().map(data => 
+                { this.state && this.state.articles && this.state.articleCount && (
+                  articleCount--,
                     <Card 
-                      title={data.title} 
-                      description={data.description}
-                      image={data.image}
-                      url={data.url}
+                      title={articles[articleCount].title} 
+                      description={articles[articleCount].description}
+                      image={articles[articleCount].image}
+                      url={articles[articleCount].url}
                       />
-                  )
-                  
+                )   
+                }
+                { this.state && this.state.articles && this.state.articleCount && (
+                  articleCount--,
+                    <Card 
+                      title={articles[articleCount].title} 
+                      description={articles[articleCount].description}
+                      image={articles[articleCount].image}
+                      url={articles[articleCount].url}
+                      />
+                )   
+                }
+                { this.state && this.state.articles && this.state.articleCount && (
+                  articleCount--,
+                    <Card 
+                      title={articles[articleCount].title} 
+                      description={articles[articleCount].description}
+                      image={articles[articleCount].image}
+                      url={articles[articleCount].url}
+                      />
+                )   
                 }
                 </div>
               </div>
