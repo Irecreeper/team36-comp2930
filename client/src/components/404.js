2fa0c86986999c7e4189175598fc32fd88bd915a
Paolo Del Rio <pdelrio@csdr.ca>
2019-05-21 15:50:46 -0700

    improved the 404 page, making it more appealing

--- client/src/components/404.js
+++ client/src/components/404.js
@@ -5,6 +5,7 @@ Appears when you head to a page that doesn't exist.
 
 import React from 'react'
 import NavBar from './navbar.js'
+import Button404 from './404_button.js'
 
 class Contact extends React.Component {
   constructor(props) {
@@ -19,17 +20,22 @@ class Contact extends React.Component {
       <div>
         <NavBar />
         <div class="contain">
-          <h1 class="text-center text-white font-el">404 Baby</h1>
+          <h1 class="text-center text-on-background-lg font-el">404: Page Not Found</h1>
           <div id="main-content" class="container">
-            Looks like that page doesn't exist friendo!
-            <br />
-            Either that, or Manuel hasn't fixed the routing issue yet >:@
-            <br />
-            Please be patient with us.
-            <br /><br />
-            For further viewing of VEN's project scope and design, check out our
-            project pitch on <a href="https://urlzs.com/WQTA">YouTube</a>.
-
+            <p>
+              The URL you tried to access did not exist on our servers. Perhaps you followed 
+              a faulty link, tried to input a link directly in the URL, or something on our
+              end broke? We can't really be sure, sorry!
+            </p>
+            <p>
+              Fortunately, your journey doesn't end here! You can either make use of the Navigation
+              Bar above to go back to the website, and read some amazing articles! Or perhaps, you 
+              can just click on this button down here to return to the landing page. It doesn't really
+              matter too much.
+            </p>
+            <div className="d-flex justify-content-center">
+              <Button404 />
+            </div>
           </div>
         </div>
       </div>
---
+++ client/src/components/404_button.js
@@ -0,0 +1,35 @@
+/* 
+A special button that appears on the 404 page.
+Returns you to the Landing Page.
+*/
+
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+class About_us_but extends Component {
+  constructor(props) {
+    super(props)
+
+    this.go = this.go.bind(this)
+  }
+
+  //REMEMBER: There must be a proper "route" established in App.js's <Switch> area!
+  go() {
+    this.props.history.push('/')
+  }
+
+  render() {
+    return (
+        <button
+          id="view_button"
+          type="button"
+          class="btn btn-success font_ro"
+          onClick={this.go}
+        >
+          Return to Landing
+        </button>
+    )
+  }
+}
+
+export default withRouter(About_us_but)
