cee4687bdb3d514525591a55066273d4772aa652
$USER_NAME <$USER_EMAIL>
2019-05-22 20:03:31 -0700

    fixed db loading during unmount issue

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -25,85 +25,113 @@ const style = {
 }
 
 class Main extends React.Component {
-  
+
   state = {
     items: Array.from({ length: 2 }),
     hasMore: true
-    
+
   }
 
   switchToEnergy = () => {
-    this.setState({articles: this.state.articlesEnergy});
-    this.setState({articleCount: this.state.articleCountEnergy})
+    this.setState({ articles: this.state.articlesEnergy });
+    this.setState({ articleCount: this.state.articleCountEnergy })
     console.log("Changed to energy articles");
-    this.setState({items: Array.from({ length: 0 })})
-    this.setState({hasMore: true});
+    this.setState({ items: Array.from({ length: 0 }) })
+    this.setState({ hasMore: true });
     this.fetchMoreData();
   }
 
   switchToPollution = () => {
-    this.setState({articles: this.state.articlesPollution});
-    this.setState({articleCount: this.state.articleCountPollution})
+    this.setState({ articles: this.state.articlesPollution });
+    this.setState({ articleCount: this.state.articleCountPollution })
     console.log("Changed to pollution articles");
-    this.setState({items: Array.from({ length: 0 })})
-    this.setState({hasMore: true});
+    this.setState({ items: Array.from({ length: 0 }) })
+    this.setState({ hasMore: true });
     this.fetchMoreData();
   }
 
   switchToRecycling = () => {
-    this.setState({articles: this.state.articlesRecycling});
-    this.setState({articleCount: this.state.articleCountRecycling})
+    this.setState({ articles: this.state.articlesRecycling });
+    this.setState({ articleCount: this.state.articleCountRecycling })
     console.log("Changed to recycling articles");
-    this.setState({items: Array.from({ length: 0 })})
-    this.setState({hasMore: true});
+    this.setState({ items: Array.from({ length: 0 }) })
+    this.setState({ hasMore: true });
     this.fetchMoreData();
   }
 
   switchToAll = () => {
-    this.setState({articles: this.state.articlesAll});
-    this.setState({articleCount: this.state.articleCountAll})
+    this.setState({ articles: this.state.articlesAll });
+    this.setState({ articleCount: this.state.articleCountAll })
     console.log("Changed to all articles");
-    this.setState({items: Array.from({ length: 0 })})
-    this.setState({hasMore: true});
+    this.setState({ items: Array.from({ length: 0 }) })
+    this.setState({ hasMore: true });
     this.fetchMoreData();
+  }
+
+
+  componentWillUnmount() {
+    this.mounted = false;
+    console.log("unmounted");
   }
 
   //
   componentDidMount() {
+
+    this.mounted = true;
+    console.log("mounted");
+
+
     this.getDbArticles().then(data => {
-      this.setState({articlesAll: data});
-      this.setState({articleCountAll: this.state.articlesAll.length});
+
+      if (this.mounted)
+        this.setState({ articlesAll: data });
+
+      if (this.mounted)
+        this.setState({ articleCountAll: this.state.articlesAll.length });
       console.log(this.state.articlesAll);
       console.log("Number of unsorted articles: " + this.state.articleCountAll);
     })
-    .then(() => {
-      if (this.props.location.default == 'energy') {
-        this.switchToEnergy();
-      } else if (this.props.location.default == 'pollution') {
-        this.switchToPollution();
-      } else if (this.props.location.default == 'recycling') {
-        this.switchToRecycling();
-      } else this.switchToAll();
-      
-    })
+      .then(() => {
+
+        if (this.props.location.default == 'energy') {
+          this.switchToEnergy();
+        } else if (this.props.location.default == 'pollution') {
+          this.switchToPollution();
+        } else if (this.props.location.default == 'recycling') {
+          this.switchToRecycling();
+        } else this.switchToAll();
+
+      })
 
     this.getDbArticlesEnergy().then(data => {
-      this.setState({articlesEnergy: data});
-      this.setState({articleCountEnergy: this.state.articlesEnergy.length});
+
+      if (this.mounted)
+        this.setState({ articlesEnergy: data });
+
+      if (this.mounted)
+        this.setState({ articleCountEnergy: this.state.articlesEnergy.length });
       console.log(this.state.articlesEnergy);
       console.log("Number of energy articles: " + this.state.articleCountEnergy);
     })
 
     this.getDbArticlesPollution().then(data => {
-      this.setState({articlesPollution: data});
-      this.setState({articleCountPollution: this.state.articlesPollution.length});
+
+      if (this.mounted)
+        this.setState({ articlesPollution: data });
+
+      if (this.mounted)
+        this.setState({ articleCountPollution: this.state.articlesPollution.length });
       console.log(this.state.articlesPollution);
       console.log("Number of pollution articles: " + this.state.articleCountPollution)
     })
 
     this.getDbArticlesRecycling().then(data => {
-      this.setState({articlesRecycling: data});
-      this.setState({articleCountRecycling: this.state.articlesRecycling.length});
+
+      if (this.mounted)
+        this.setState({ articlesRecycling: data });
+
+      if (this.mounted)
+        this.setState({ articleCountRecycling: this.state.articlesRecycling.length });
       console.log(this.state.articlesRecycling);
       console.log("Number of recycling articles: " + this.state.articleCountRecycling);
     })
@@ -114,22 +142,22 @@ class Main extends React.Component {
       this.setState({
         items: this.state.items.concat(Array.from({ length: 2 })),
       })
-      console.log(this.state.items.length*3);
+      console.log(this.state.items.length * 3);
       console.log(this.state.articleCount - 6)
       console.log(this.state.hasMore)
-      if (this.state.items.length*3 > this.state.articleCount - 6) {
-        this.setState({hasMore: false});
+      if (this.state.items.length * 3 > this.state.articleCount - 6) {
+        this.setState({ hasMore: false });
       }
     }, 500)
   }
 
   //Get all articles from cache
   getDbArticles() {
-  
+
     return new Promise((resolve, reject) => {
       console.log("Running getDbArticles");
       axios
-      .get('http://localhost:9000/api/')
+        .get('http://localhost:9000/api/')
         .then(response => {
           resolve(response.data);
           return;
@@ -194,16 +222,16 @@ class Main extends React.Component {
 
   //Fill cards with articles
   render() {
-    
+
     let articles = this.state.articles;
     var articleCount = this.state.articleCount;
     return (
       <div id="container">
         <NavBar
-          callback = {this.switchToEnergy}
-          callback2 = {this.switchToPollution}
-          callback3 = {this.switchToRecycling}
-          callback4 = {this.switchToAll}
+          callback={this.switchToEnergy}
+          callback2={this.switchToPollution}
+          callback3={this.switchToRecycling}
+          callback4={this.switchToAll}
         />
         <InfiniteScroll
           dataLength={this.state.items.length}
@@ -214,43 +242,43 @@ class Main extends React.Component {
             <div key={index}>
               <div className="container">
                 <div id="news-card-area" class="row">
-                { this.state && this.state.articles && this.state.articleCount && (
-                  articleCount--,
-                    <Card 
+                  {this.state && this.state.articles && this.state.articleCount && (
+                    articleCount-- ,
+                    <Card
                       _id={articles[articleCount]._id}
-                      title={articles[articleCount].title} 
+                      title={articles[articleCount].title}
                       description={articles[articleCount].description}
                       image={articles[articleCount].image}
                       url={articles[articleCount].url}
-                      />
-                )   
-                }
-                { this.state && this.state.articles && this.state.articleCount && (
-                  articleCount--,
-                    <Card 
+                    />
+                  )
+                  }
+                  {this.state && this.state.articles && this.state.articleCount && (
+                    articleCount-- ,
+                    <Card
                       _id={articles[articleCount]._id}
-                      title={articles[articleCount].title} 
+                      title={articles[articleCount].title}
                       description={articles[articleCount].description}
                       image={articles[articleCount].image}
                       url={articles[articleCount].url}
-                      />
-                )   
-                }
-                { this.state && this.state.articles && this.state.articleCount && (
-                  articleCount--,
-                    <Card 
+                    />
+                  )
+                  }
+                  {this.state && this.state.articles && this.state.articleCount && (
+                    articleCount-- ,
+                    <Card
                       _id={articles[articleCount]._id}
-                      title={articles[articleCount].title} 
+                      title={articles[articleCount].title}
                       description={articles[articleCount].description}
                       image={articles[articleCount].image}
                       url={articles[articleCount].url}
-                      />
-                )   
-                }
+                    />
+                  )
+                  }
                 </div>
               </div>
-            </div>         
-        ))}
+            </div>
+          ))}
         </InfiniteScroll>
       </div>
     )
